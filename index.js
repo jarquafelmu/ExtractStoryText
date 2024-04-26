c5eed7f431594af81978c740af0b7915242a2197
Sam Terrazas <thechaoselement@gmail.com>
2024-04-25 22:40:49 -0600

    makes the process async

--- index.js
+++ index.js
@@ -1,55 +1,57 @@
-import fs, { lstatSync } from "fs";
-import { rm } from "node:fs/promises";
+// import fs, { lstatSync } from "fs";
+import * as fs from "fs/promises";
 import { load } from "cheerio";
 import sanitize from "sanitize-filename";
-import decode from "decode-html";
 
+// Configuration
 const REMOVE_FILE = false;
 const EXTRACT_AS_HTML = true;
-const SITE = 'royalroad'; // royalroad or ga
+const SITE = "royalroad"; // royalroad or ga
 
 const siteHandlers = {
-  royalroad: { title: "title", content: ".chapter-content > p, .chapter-content table" },
-  ga: { title: "#chapterTitle", content: "#chapterText > p, #chapterText table" },
+  royalroad: {
+    title: "title",
+    content: ".chapter-content > p, .chapter-content table",
+  },
+  ga: {
+    title: "#chapterTitle",
+    content: "#chapterText > p, #chapterText table",
+  },
 };
-
-/**
- * Extracts text from an HTML element.
- *
- * @param {Object} $ - The jQuery object.
- * @param {HTMLElement} el - The HTML element to extract text from.
- * @param {boolean} [extractAsHTML=false] - Whether to extract the text as HTML or plain text. Default is false.
- * @returns {string} - The extracted text.
- */
-function extractText($, el, extractAsHTML = false) {
-  if (extractAsHTML) return $(el).html();
-  return $(el).text();
-}
 
 /**
  * Process the file at the given file path.
  * @param {string} filePath - The path of the file to be processed.
  */
-function process(filePath) {
-  const file = fs.readFileSync(filePath, "utf8");
-  const $ = load(file);
+async function process(filePath) {
+  try {
+    const file = await fs.readFile(filePath, "utf8");
+    const $ = load(file);
 
-  const sanitizedTitle = sanitize($(siteHandlers[SITE].title).text().trim()); // sanitize the title to be used as a filename
-  
-  const storyContent = $(siteHandlers[SITE].content)
-    .toArray()
-    .map((el) =>
-      decode( // decode HTML entities
-        extractText($, el, EXTRACT_AS_HTML) // extract text from the element
-          .replace(/“|”/g, '"') // replace smart quotes
-          .replace(/’/g, "'") // replace smart apostrophes
-          .replace(/\n(?= )|(?<= )\n/g, "") // remove newlines between words
-          .replace(/\n{3,}/g, "\n\n") // remove multiple newlines
-      )
-    )
-    .join("\n\n");
+    const title = sanitize($(siteHandlers[SITE].title).text().trim()); // sanitize the title to be used as a filename
 
-  writeFile(`${sanitizedTitle}`, storyContent);
+    const content = $(siteHandlers[SITE].content)
+      .toArray()
+      .map((el) => {
+        let text = EXTRACT_AS_HTML ? $(el).html() : $(el).text();
+        text = text.replace(/“|”/g, '"'); // replace smart quotes
+        text = text.replace(/‘|’/g, "'"); // replace smart apostrophes
+        text = text.replace(/\n(?= )|(?<= )\n/g, ""); // remove newlines between words
+        text = text.replace(/\n{3,}/g, "\n\n"); // remove multiple newlines
+
+        return decodeURI(text); // decode HTML entities
+      })
+      .join("\n\n");
+
+    await writeFile(`${title}`, content, EXTRACT_AS_HTML ? "html" : "txt");
+
+    if (REMOVE_FILE) {
+      // remove the file
+      await fs.rm(filePath, { recursive: true, force: true });
+    }
+  } catch (err) {
+    console.error(`Failed to process file ${filePath}: ${err.message}`);
+  }
 }
 
 /**
@@ -57,28 +59,20 @@ function process(filePath) {
  * @param {string} dirPath - The path of the directory to read.
  */
 async function readDir(dirPath) {
-  fs.readdir(dirPath, (err, files) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    files.forEach(async (file) => {
-      const filepath = `${dirPath}${file}`;
-      const stat = lstatSync(filepath);
+  try {
+    const files = await fs.readdir(dirPath);
+    for (const file of files) {
+      const filepath = `${dirPath}/${file}`;
+      const stat = await fs.stat(filepath);
 
       // skip directories
       if (!stat.isDirectory()) {
-        process(filepath);
+        await process(filepath);
       }
-
-      // exit if we don't want to remove the file
-      if (!REMOVE_FILE) return;
-
-      // remove the file
-      await rm(filepath, { recursive: true, force: true });
-    });
-  });
+    }
+  } catch (err) {
+    console.error(`Failed to read directory ${filepath}: ${err.message}`);
+  }
 }
 
 /**
@@ -88,11 +82,19 @@ async function readDir(dirPath) {
  * @param {string} [outDir='output'] - The directory where the file will be created. Defaults to 'output'.
  * @param {string} [extension='txt'] - The file extension. Defaults to 'txt'.
  */
-function writeFile(filename, content, outDir = "extracted", extension = "txt") {
-  if (!fs.existsSync(outDir)) {
-    fs.mkdirSync(outDir);
+async function writeFile(
+  filename,
+  content,
+  extension = "txt",
+  outDir = "extracted"
+) {
+  try {
+    await fs.access(outDir);
+  } catch (e) {
+    await fs.mkdir(outDir);
   }
-  fs.writeFileSync(`${outDir}/${filename}.${extension}`, content);
+
+  await fs.writeFile(`${outDir}/${filename}.${extension}`, content);
 }
 
-readDir("raw/");
+readDir("raw");
